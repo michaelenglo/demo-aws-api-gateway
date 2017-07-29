987af33350254cbc37064a99847de8199bc40541
Michael Englo <menglo@optimalefficiency.net>
2017-07-28 17:25:44 -0700

    add ajax call + put some value into html

--- v/script.js
+++ v/script.js
@@ -1,4 +1,16 @@
+var EMOTION_URL = 'http://demoawsapigatewaysa-dot-michaelenglo01.appspot.com';
 
 $(function () {
-
+	$('.eAnalyzeEmotion').on('click', function() {
+		var content =  $(".postContent").text();
+		console.log(content);
+		var posting = $.post(EMOTION_URL + '/', {
+			'text' : content
+		});
+	
+		posting.done(function(data) {
+			$('#eAnalyzeEmotion1').remove();
+			$('#emotionColumn1').append("<strong> score: " + data.score + "</strong>");
+		});
+	});
 });
